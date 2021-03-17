33e8de69324b64a2eef239ad0f74e6ae8921fa15
xdeepakv <deepak.m.shrma@gmail.com>
2021-03-17 21:41:23 +0800

    added webpack code

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.tsx",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
